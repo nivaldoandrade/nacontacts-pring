12ceb48937b53f5c49ff5d9d7ece76451ace6900
nivaldoandrade <nivaldoandradef@gmail.com>
2024-02-14 12:47:44 -0300

    chore: initialize project

---
+++ src/main/java/com/nasa/nacontacts/NacontactsApplication.java
@@ -0,0 +1,13 @@
+package com.nasa.nacontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacontactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacontactsApplication.class, args);
+	}
+
+}
